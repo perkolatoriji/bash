d7fe37bd28f18f3d5c7bab2a2fdf4f8b3f943e95
Perkolator <67321158+perkolatoriji@users.noreply.github.com>
2020-11-19 10:17:22 +0100

    Update plantilla.sh
    added ACTION instead of siOK, so now we can abort script if_error or continue (both with an error msg). More granular now.

--- plantilla.sh
+++ plantilla.sh
@@ -16,20 +16,21 @@
 # -----------    ----------   -------------------------------------------
 # Version=1.00   08/07/2013 - Carlos Ijalba, Original.
 # Version=1.02   23/10/2015 - Carlos Ijalba, Reemplazado echos por "echo -e".
-  Version=1.03 # 24/04/2018 - Carlos Ijalba, Añadido basename en $PROGRAMA.
+# Version=1.03   24/04/2018 - Carlos Ijalba, Añadido basename en $PROGRAMA.
+  Version=1.04 # 19/11/2020 - Carlos Ijalba, Añadido return & exit to if_error funtion. 
 #
 #########################################################################
 
-#Debug                                Par�metros para Depurar c�digo ####
+#Debug                                Parametros para Depurar codigo ####
 
 # set -x
 
 
-#Var                                        Declaraci�n de Variables ####
+#Var                                        Declaracion de Variables ####
 
 PROGRAMA=$(basename $0)                 # declarar las variables de nuestros scripts
 TEMP=/tmp/file                          # siempre en MAYUSCULAS, para poder identificarlas
-                                        # f�cilmente dentro del c�digo
+                                        # facilmente dentro del codigo
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
@@ -42,7 +43,7 @@ ENDC="\033[m"
 function usage
  ######################
  #
- # funci�n para mostrar la pantalla de ayuda del script (usage).
+ # funcion para mostrar la pantalla de ayuda del script (usage).
  #
  ######################
 {
@@ -62,30 +63,32 @@ EOF
 function if_error
  ######################
  #
- # funci�n para tratar errores despu�s de la ejecuci�n de un comando,
+ # funcion para tratar errores despues de la ejecucion de un comando,
  # se invoca de la siguiente manera:
  #
- #    funci�n         $1            $2          $3
- #    -------- -------------------  ---- ------------------------
- #    if_error "Fallo de operaci�n" siOK "Funcion� correctamente"
+ #    funcion         $1              $2              $3
+ #    -------- -------------------  ------  ------------------------
+ #    if_error "Fallo de operacion" ACTION  "Funciono correctamente"
  #
- # Nota: en $2 podemos usar siOK, else, o cualquier otra palabra.
+ # Nota: ACTION ($2) es la accion a tomar cuando la operacion anterior ha fallado, esta puede ser "exit" o "return", dependiendo
+ # de si queremos que con un error el programa aborte con el msg de error y el RC o que solamente de el msg de error y siga su
+ # ejecucion.
  #
  ######################
 {
 if [ $? -ne 0 ]
-  then                                  # comprobar el �ltimo return code, si rc <> 0...
+  then                                  # comprobar el ultimo return code, si rc <> 0...
     echo -e "$RED--- ERROR: $ENDC$1\n"  # ...imprimir msg y devolver el rc original.
-    exit $?
-  else                                  # si fu� OK, imprimir msg satisfactorio.
+    $2 $?
+  else                                  # si fue OK, imprimir msg satisfactorio.
     echo -e "$GREEN+++ OK: $ENDC$3"
 fi
 }
 
-#Usage                                         Mostrar uso y versi�n ####
+#Usage                                         Mostrar uso y version ####
 
-                                        # Si ejecutamos el script sin los par�metros correctos...
-if [ $# -eq 0 ]                         # ...mostrar el uso correcto y versi�n del programa.
+                                        # Si ejecutamos el script sin los parametros correctos...
+if [ $# -eq 0 ]                         # ...mostrar el uso correcto y version del programa.
   then
     usage
     exit 1
@@ -97,7 +100,7 @@ fi
 echo -e "\n O-> $PROGRAMA v$Version - Plantilla para programas bash shell.\n"
 
 rm $TEMP > /dev/null 2>&1
-if_error  "Fallo al intentar borrar fichero $TEMP."  siOK  "Borrado fichero $TEMP."
+if_error "Fallo al intentar borrar fichero $TEMP." return "Borrado fichero $TEMP."
 
 echo -e "\n o-> $PROGRAMA Finished.\n"     # msg final
 
